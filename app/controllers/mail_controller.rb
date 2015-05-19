09d3e623b691f7469a52a82420b643ece395b023
Bode <ella92mp@yahoo.com>
2015-05-19 16:04:18 +0300

    User management + cart + mailing + cart deletion

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -2,5 +2,6 @@ class MailController<ApplicationController
 	def test
 		user = User.first
 		UserMailer.welcome_email("ella92mp@yahoo.com").deliver_now
+		$redis.del "cart#{current_user.id}"
 	end
 end
