bfc2a73504fb50e5ed24a23281e343e1fb05cd85
bodebogdan <ella92mp@yahoo.com>
2015-07-08 15:08:15 +0300

    before heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,6 @@ class UserMailer < ApplicationMailer
 			@user = user 
 			@user_products = products
 			@url = 'http:yahoo.com'
-			file=File.join('/home','svuser','workspace','sv-labs-2015','apple-touch-icon.png')
-			attachments['testfile.png'] = File.read(file)
 			
 			@user_message = message
 			mail(to: @user,
